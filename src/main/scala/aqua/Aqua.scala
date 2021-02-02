4f90c22dd2166a84427c4ecfed748d18ecd9cc1b
dmitry <dmitry@fluence.one>
2021-02-02 16:56:47 +0300

    example.aqua

--- src/main/scala/aqua/Aqua.scala
+++ src/main/scala/aqua/Aqua.scala
@@ -1,7 +1,15 @@
 package aqua
 
-import aqua.parse.Block
+import aqua.parse.{ArrowType, Block, DataType, Type}
+import cats.data.NonEmptyMap
 import cats.parse.{Parser ⇒ P, Parser0 ⇒ P0}
+
+case class Aqua(
+                 inputs: Map[String, DataType],
+                 callbacks: Map[String, ArrowType],
+                 types: Map[String, Type],
+                 services: Map[String, NonEmptyMap[String, ArrowType]]
+               )
 
 object Aqua {
  import aqua.parse.Token._
