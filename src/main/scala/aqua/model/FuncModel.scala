dbcacebdbdc8ce5927205d1b8f9a39dfd2cb1605
dmitry <dmitry@fluence.one>
2021-03-19 16:42:12 +0300

    Move typescript-specific things to Gen

--- src/main/scala/aqua/model/FuncModel.scala
+++ src/main/scala/aqua/model/FuncModel.scala
@@ -1,10 +1,18 @@
 package aqua.model
 
 import aqua.generator.DataView.{InitPeerId, StringScalar}
-import aqua.generator.{Air, AirContext, AirGen, ArrowCallable, DataView, FuncCallable, SrvCallableOnPeer}
-import aqua.semantics.{ArrayType, ArrowType, DataType, Type}
+import aqua.generator.{
+  Air,
+  AirContext,
+  AirGen,
+  ArrowCallable,
+  DataView,
+  FuncCallable,
+  SrvCallableOnPeer,
+  TypescriptFunc
+}
+import aqua.semantics.{ArrowType, DataType, Type}
 import cats.data.{Chain, NonEmptyChain}
-import cats.syntax.show._
 
 case class FuncModel(
   name: String,
@@ -20,26 +28,6 @@ case class FuncModel(
 
   def callable: ArrowCallable =
     new FuncCallable(args, ret.map(_._1), bodyGen)
-
-  def typeToTs(t: Type): String = t match {
-    case ArrayType(t) => typeToTs(t) + "[]"
-    case dt: DataType => "any" // TODO render types
-    case at: ArrowType =>
-      s"(${argsToTs(at)}) => ${at.res
-        .fold("void")(_ => "any")}"
-  }
-
-  def argsToTs(at: ArrowType): String =
-    at.args.map(typeToTs).zipWithIndex.map(_.swap).map(kv => "arg" + kv._1 + ": " + kv._2).mkString(", ")
-
-  def argsCallToTs(at: ArrowType): String =
-    at.args.zipWithIndex.map(_._2).map("arg" + _).mkString(", ")
-
-  def argsTypescript: String =
-    args.map {
-      case (n, Left(t)) => s"${n}: " + typeToTs(t)
-      case (n, Right(at)) => s"${n}: " + typeToTs(at)
-    }.mkString(", ")
 
   def airContext(acc: Map[String, ArrowCallable]): AirContext =
     AirContext(
@@ -57,6 +45,45 @@ case class FuncModel(
       .generate(airContext(acc))
       ._2
 
+  def generateTs(acc: Map[String, ArrowCallable]): TypescriptFunc =
+    TypescriptFunc(this, generateTsAir(acc))
+
+  val respFuncName = "response"
+
+  val returnCallback: Option[FuncOp] = ret.map { case (dv, t) =>
+    viaRelay(
+      CoalgebraModel(
+        Some(ServiceModel(callbackService, StringScalar("\"" + callbackService + "\""))),
+        respFuncName,
+        (dv, t) :: Nil,
+        None
+      )
+    )
+
+  }
+
+  def generateTsAir(acc: Map[String, ArrowCallable]): Air =
+    SeqModel(
+      NonEmptyChain
+        .fromChainAppend(
+          Chain.fromSeq(
+            args.collect { case (argName, Left(_)) =>
+              getDataOp(argName)
+            } :+ getDataOp("relay")
+          ),
+          body
+        )
+        .appendChain(Chain.fromSeq(returnCallback.toSeq))
+    ).toAirGen.generate(airContext(acc))._2
+
+  def getDataOp(name: String): FuncOp =
+    CoalgebraModel(
+      Some(ServiceModel(getDataService, StringScalar("\"" + getDataService + "\""))),
+      name,
+      Nil,
+      Some(name)
+    )
+
   def viaRelay(op: FuncOp): FuncOp =
     OnModel(
       DataView.Variable("relay"),
@@ -72,90 +99,5 @@ case class FuncModel(
         )
       )
     )
-
-  def generateTypescript(acc: Map[String, ArrowCallable]): String = {
-    def getDataOp(name: String): FuncOp =
-      CoalgebraModel(
-        Some(ServiceModel(getDataService, StringScalar("\"" + getDataService + "\""))),
-        name,
-        Nil,
-        Some(name)
-      )
-
-    val returnCallback = ret.map { case (dv, t) =>
-      val respFuncName = "response"
-      viaRelay(
-        CoalgebraModel(
-          Some(ServiceModel(callbackService, StringScalar("\"" + callbackService + "\""))),
-          respFuncName,
-          (dv, t) :: Nil,
-          None
-        )
-      ) ->
-        s"""h.on('$callbackService', '$respFuncName', (args) => {
-           |  const [res] = args;
-           |  resolve(res);
-           |});
-           |""".stripMargin
-
-    }
-
-    val air = SeqModel(
-      NonEmptyChain
-        .fromChainAppend(
-          Chain.fromSeq(
-            args.collect { case (argName, Left(_)) =>
-              getDataOp(argName)
-            } :+ getDataOp("relay")
-          ),
-          body
-        )
-        .appendChain(Chain.fromSeq(returnCallback.map(_._1).toSeq))
-    ).toAirGen.generate(airContext(acc))._2.show
-
-    val setCallbacks = args.map {
-      case (argName, Left(t)) =>
-        s"""h.on('$getDataService', '$argName', () => {return $argName;});"""
-      case (argName, Right(at)) =>
-        s"""h.on('$callbackService', '$argName', (${argsToTs(at)}) => {return $argName(${argsCallToTs(at)});});"""
-    }.mkString("\n")
-
-    s"""
-       |export async function ${name}(client: FluenceClient, ${argsTypescript}): Promise<${ret
-      .map(_._2)
-      .fold("void")(typeToTs)}> {
-       |    let request;
-       |    const promise = new Promise<string>((resolve, reject) => {
-       |        request = new RequestFlowBuilder()
-       |            .withRawScript(
-       |                `
-       |$air
-       |            `,
-       |            )
-       |            .configHandler((h) => {
-       |                h.on('${getDataService}', 'relay', () => {
-       |                    return client.relayPeerId;
-       |                });
-       |                h.on('getRelayService', 'hasReleay', () => {// Not Used
-       |                    return client.relayPeerId !== undefined;
-       |                });
-       |                $setCallbacks
-       |                ${returnCallback.map(_._2).getOrElse("")}
-       |                h.on('nameOfServiceWhereToSendXorError', 'errorProbably', (args) => {
-       |                    // assuming error is the single argument
-       |                    const [err] = args;
-       |                    reject(err);
-       |                });
-       |            })
-       |            .handleTimeout(() => {
-       |                reject('message for timeout');
-       |            })
-       |            .build();
-       |    });
-       |    await client.initiateFlow(request);
-       |    return promise;
-       |}
-      """.stripMargin
-  }
 
 }
--- src/main/scala/aqua/model/ScriptModel.scala
+++ src/main/scala/aqua/model/ScriptModel.scala
@@ -1,6 +1,6 @@
 package aqua.model
 
-import aqua.generator.ArrowCallable
+import aqua.generator.{ArrowCallable, TypescriptFile, TypescriptFunc}
 
 import scala.collection.immutable.Queue
 import cats.syntax.show._
@@ -20,14 +20,13 @@ case class ScriptModel(funcs: Queue[FuncModel]) extends Model {
       ._2
 
   def generateTypescript: String =
-    """import { FluenceClient, PeerIdB58 } from '@fluencelabs/fluence';
-      |import { RequestFlowBuilder } from '@fluencelabs/fluence/dist/api.unstable';
-      |
-      |""".stripMargin ++ funcs
-      .foldLeft((Map.empty[String, ArrowCallable], Queue.empty[String])) { case ((funcsAcc, outputAcc), func) =>
-        funcsAcc.updated(func.name, func.callable) -> outputAcc.enqueue(func.generateTypescript(funcsAcc))
-      }
-      ._2
-      .mkString("\n\n")
+    TypescriptFile(
+      funcs
+        .foldLeft((Map.empty[String, ArrowCallable], Queue.empty[TypescriptFunc])) {
+          case ((funcsAcc, outputAcc), func) =>
+            funcsAcc.updated(func.name, func.callable) -> outputAcc.enqueue(func.generateTs(funcsAcc))
+        }
+        ._2
+    ).show
 
 }
