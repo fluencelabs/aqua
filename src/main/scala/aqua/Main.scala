6ff201fa4cb57191a74b09a64d45284ca8e0f83c
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-03-24 11:49:21 +0300

    improve args parsing

--- src/main/scala/aqua/Main.scala
+++ src/main/scala/aqua/Main.scala
@@ -1,62 +1,13 @@
 package aqua
 
 import aqua.cli.AquaGen.{convertAqua, convertAquaFilesToDir}
-import aqua.cli.{AquaScriptErrors, CliArgsError, CliError, IOError}
+import aqua.cli.{AquaScriptErrors, ArgsConfig, CliArgsError, CliError, IOError}
 import cats.effect.{ExitCode, IO, IOApp, Resource}
-import scopt.OParser
-
-import java.io.File
-import java.nio.file.Path
 
 final case class ParseArgsException(private val message: String, private val cause: Throwable = None.orNull)
     extends Exception(message, cause)
 
-case class Config(input: Option[String] = None, output: Option[String] = None, debug: Boolean = false)
-
 object Main extends IOApp {
-
-  private val builder = OParser.builder[Config]
-
-  private val parser1 = {
-    import builder._
-    OParser.sequence(
-      programName("ahc"),
-      head("ahc", "0.1"),
-      opt[Boolean]('d', "debug")
-        .action((x, c) => c.copy(debug = x))
-        .text("debug mode (not implemented)"),
-      opt[String]('i', "input")
-        .action((x, c) => c.copy(input = Some(x)))
-        .text("path to directory with aquamarine files"),
-      opt[String]('o', "output")
-        .action((x, c) => c.copy(output = Some(x)))
-        .text("path to output directory"),
-      help('h', "help").text("prints this usage text"),
-      checkConfig(c => {
-        println(c.input)
-        println(c.output)
-        if (c.input.isEmpty != c.output.isEmpty) {
-          failure("'input' and 'output' must be both specified or not specified")
-        } else success
-      })
-    )
-  }
-
-  private def parseIO(input: String, output: String): Either[ParseArgsException, (List[File], Path)] = {
-    for {
-      inputDir <- {
-        val inputDir = new File(input)
-        if (!inputDir.isDirectory && !inputDir.exists())
-          Left(ParseArgsException("Input path should be a dir and exists"))
-        else Right(inputDir)
-      }
-      outputDir <- {
-        val outputDir = new File(output)
-        if (!outputDir.isDirectory && !outputDir.exists()) Left(ParseArgsException("Output path should be a dir"))
-        else Right(outputDir)
-      }
-    } yield (inputDir.listFiles().toList, outputDir.toPath)
-  }
 
   private def showResults(results: List[Either[CliError, String]]): IO[Unit] = {
     IO {
@@ -97,15 +48,13 @@ object Main extends IOApp {
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
-    OParser.parse(parser1, args, Config()) match {
+    ArgsConfig.parseArgs(args) match {
       case Some(config) =>
         val io = for {
           results <- {
             (config.input, config.output) match {
               case (Some(i), Some(o)) =>
-                IO.fromEither(parseIO(i, o)).flatMap { case (files, outputDir) =>
-                  convertAquaFilesToDir[IO](files, outputDir)
-                }
+                convertAquaFilesToDir[IO](i, o)
               case _ =>
                 readAllInput().map(i => {
                   if (i.isEmpty) {
---
+++ src/main/scala/aqua/cli/ArgsConfig.scala
@@ -0,0 +1,57 @@
+package aqua.cli
+
+import scopt.{OParser, Read}
+import scopt.Read.stringRead
+
+import java.io.File
+import java.nio.file.Path
+
+case class Config(input: Option[List[File]] = None, output: Option[Path] = None, debug: Boolean = false)
+
+object ArgsConfig {
+
+  implicit val filesListRead: Read[List[File]] =
+    stringRead.map { str =>
+      val inputDir = new File(str)
+      if (!inputDir.isDirectory && !inputDir.exists())
+        throw new IllegalArgumentException(s"Input path '$str' should be a dir and exists")
+      else
+        inputDir.listFiles().toList
+    }
+
+  implicit val pathRead: Read[Path] =
+    stringRead.map { str =>
+      val outputDir = new File(str)
+      if (!outputDir.isDirectory && !outputDir.exists())
+        throw new IllegalArgumentException(s"Input path '$str' should be a dir and exists")
+      else
+        outputDir.toPath
+    }
+
+  private val builder = OParser.builder[Config]
+
+  val argParser = {
+    import builder._
+    OParser.sequence(
+      programName("ahc"),
+      head("ahc", "0.1"),
+      opt[Boolean]('d', "debug")
+        .action((x, c) => c.copy(debug = x))
+        .text("debug mode (not implemented)"),
+      opt[List[File]]('i', "input")
+        .action((x, c) => c.copy(input = Some(x)))
+        .text("path to directory with aquamarine files"),
+      opt[Path]('o', "output")
+        .action((x, c) => c.copy(output = Some(x)))
+        .text("path to output directory"),
+      help('h', "help").text("prints this usage text"),
+      checkConfig(c => {
+        if (c.input.isEmpty != c.output.isEmpty) {
+          failure("'input' and 'output' must be both specified or not specified")
+        } else success
+      })
+    )
+  }
+
+  def parseArgs(args: List[String]): Option[Config] = OParser.parse(argParser, args, Config())
+}
