5e4f02a7200a9965f3b59724b260a36d474f13df
dmitry <dmitry@fluence.one>
2021-04-06 17:05:02 +0300

    Get more context into topology resolver

--- model/src/main/scala/aqua/model/transform/Topology.scala
+++ model/src/main/scala/aqua/model/transform/Topology.scala
@@ -1,25 +1,30 @@
 package aqua.model.transform
 
-import aqua.model.body.{CallServiceTag, FuncOp, OnTag, OpTag}
+import aqua.model.ValueModel
+import aqua.model.body.{CallServiceTag, FuncOp, OnTag, OpTag, SeqTag}
 import cats.Eval
 import cats.data.Chain
 import cats.free.Cofree
-import cats.syntax.apply._
 
 object Topology {
   type Tree = Cofree[Chain, OpTag]
 
+  def through(peers: List[ValueModel]): Chain[Tree] =
+    Chain.fromSeq(
+      peers.map(FuncOp.noop).map(_.tree)
+    )
+
   // TODO: after topology is resolved, OnTag should be eliminated
   def resolve(op: Tree): Tree =
     transformWithPath(op) {
-      case (path, c: CallServiceTag) if c.peerId.isEmpty =>
+      case (path, c: CallServiceTag, children) if c.peerId.isEmpty =>
         Cofree[Chain, OpTag](
           c.copy(peerId = path.collectFirst { case OnTag(peerId, _) =>
             peerId
           }),
-          Eval.now(Chain.empty)
+          children
         )
-      case (path, tag @ OnTag(pid, via)) =>
+      case (path, tag @ OnTag(pid, via), children) =>
         // Drop seq/par/xor from path
         val pathOn = path.collect { case ot: OnTag =>
           ot
@@ -28,34 +33,37 @@ object Topology {
         pathOn match {
           // If we are on the right node, do nothing
           case Nil =>
-            Cofree[Chain, OpTag](tag, Eval.now(Chain.empty))
+            Cofree[Chain, OpTag](tag, children)
           case h :: _ if h.peerId == pid =>
-            Cofree[Chain, OpTag](tag, Eval.now(Chain.empty))
+            Cofree[Chain, OpTag](tag, children)
           case h :: _ =>
             Cofree[Chain, OpTag](
               tag,
-              Eval.now(
-                Chain.fromSeq(
-                  (h.via.reverse ++ via).map(FuncOp.noop).map(_.tree)
-                )
-              )
+              // TODO: merge children, if possible
+              children.map(through(h.via.reverse ++ via) ++ _)
             )
         }
 
-      case (_, t) =>
-        Cofree[Chain, OpTag](t, Eval.now(Chain.empty))
+      case (path, SeqTag, children) =>
+        Cofree[Chain, OpTag](
+          SeqTag,
+          children.map(c =>
+            // TODO if we have OnTag, and then something else, need to get back
+            c
+          )
+        )
+
+      case (_, t, children) =>
+        Cofree[Chain, OpTag](t, children)
     }
 
   def transformWithPath(cf: Tree, path: List[OpTag] = Nil)(
-    f: (List[OpTag], OpTag) => Tree
+    f: (List[OpTag], OpTag, Eval[Chain[Tree]]) => Tree
   ): Tree = {
-    val newCf = f(path, cf.head)
+    val newCf = f(path, cf.head, cf.tail)
     Cofree[Chain, OpTag](
       newCf.head,
-      (newCf.tail, cf.tail)
-        .mapN(_ ++ _)
-        // IF make foldLeft here, will be possible to get info from prev sibling
-        .map(_.map(transformWithPath(_, newCf.head :: path)(f)))
+      newCf.tail.map(_.map(transformWithPath(_, newCf.head :: path)(f)))
     )
   }
 }
