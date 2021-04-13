871006177cc18d9298c0fdf34455ac91526d1153
Pavel Murygin <pavel.murygin@gmail.com>
2021-04-13 19:48:59 +0300

    Create aquamarine npm package

---
+++ npm/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const { exec } = require("child_process");
+
+const args = [
+  "java",
+  "-jar",
+  __dirname + "\\aquamarine.jar",
+  "-m",
+  "node_modules",
+  ...process.argv.slice(2),
+];
+
+const argsString = args.join(" ");
+
+console.log(argsString);
+exec(argsString, (err, stdout, stderr) => {
+  console.error(stderr);
+  console.log(stdout);
+
+  if (err) {
+    process.exit(err.code);
+  }
+});
