9e09f1f1b29075740e5c7d67acb8326b107ad8f8
dmitry <dmitry@fluence.one>
2021-04-19 16:14:41 +0300

    Compile bug fixed

--- model/src/main/scala/aqua/model/func/body/ShowFuncOp.scala
+++ model/src/main/scala/aqua/model/func/body/ShowFuncOp.scala
@@ -7,11 +7,11 @@ object ShowFuncOp {
 
   private def showTreeOffset(offset: Int): Show[FuncOp.Tree] = { case Cofree(head, tail) =>
     val children = tail.value
-    s"${" ".repeat(offset)}$head" +
+    s"${" " * offset}$head" +
       (if (children.isEmpty) "\n"
        else
          " {\n" + children.toList
-           .map(showTreeOffset(offset + 1).show) + s"${" ".repeat(offset)}}\n")
+           .map(showTreeOffset(offset + 1).show) + s"${" " * offset}}\n")
   }
 
   implicit val showFuncOp: Show[FuncOp] =
