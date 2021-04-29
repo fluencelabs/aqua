fc7cac964a97cff470cd9c8eba1b118a2ce6a896
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-04-29 20:12:19 +0300

    revert a little bit

--- cli/src/main/scala/aqua/Aqua.scala
+++ cli/src/main/scala/aqua/Aqua.scala
@@ -9,19 +9,13 @@ object Aqua {
   def parseString(input: String): ValidatedNec[AquaError, Ast[Span.F]] =
     Ast
       .fromString[Span.F](input)
-      .leftMap(_.map(pe => {
-        println(pe)
-        SyntaxError(pe.failedAtOffset, pe.expected)
-      }))
+      .leftMap(_.map(pe => SyntaxError(pe.failedAtOffset, pe.expected)))
 
   def parseFileString(name: String, input: String): ValidatedNec[AquaError, Ast[FileSpan.F]] = {
     implicit val fileLift: LiftParser[FileSpan.F] = FileSpan.fileSpanLiftParser(name, input)
     Ast
       .fromString[FileSpan.F](input)
-      .leftMap(_.map(pe => {
-        println(pe)
-        SyntaxError(pe.failedAtOffset, pe.expected)
-      }))
+      .leftMap(_.map(pe => SyntaxError(pe.failedAtOffset, pe.expected)))
   }
 
 }
