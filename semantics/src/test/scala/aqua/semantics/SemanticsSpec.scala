eba75972fafd0585f8635baca0909845dcfd53bc
dmitry <dmitry@fluence.one>
2021-06-16 19:17:06 +0300

    Useless test commented out

--- semantics/src/test/scala/aqua/semantics/SemanticsSpec.scala
+++ semantics/src/test/scala/aqua/semantics/SemanticsSpec.scala
@@ -1,52 +1,52 @@
 package aqua.semantics
 
-import aqua.AquaSpec
-import aqua.model.Node.{callLiteral, on, par}
-import aqua.model.transform.BodyConfig
-import aqua.model.{AquaContext, LiteralModel, Node}
-import aqua.parser.Ast
-import aqua.parser.lift.{LiftParser, Span}
-import aqua.types.LiteralType
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class SemanticsSpec extends AnyFlatSpec with Matchers with AquaSpec {
-
-  // use it to fix https://github.com/fluencelabs/aqua/issues/90
-  ignore should "create right model" in {
-    implicit val fileLift: LiftParser[Span.F] = Span.spanLiftParser
-
-    val script =
-      """service LocalPrint("srv1"):
-        |    fn1: -> ()        
-        |
-        |func topologyTest():
-        |    on "other-peer":
-        |        LocalPrint.fn1()
-        |    par LocalPrint.fn1()
-        |""".stripMargin
-
-    val ast = Ast.fromString(script).toList.head
-    val ctx = AquaContext.blank
-    val bc = BodyConfig()
-    import bc.aquaContextMonoid
-
-    val context = Semantics.process(ast, ctx).toList.head
-
-    val body = context.allFuncs()("topologyTest").body.tree.forceAll
-
-    val node = Node.cofToNode(body)
-
-    val expected =
-      par(
-        on(LiteralModel("\"other-peer\"", LiteralType.string), Nil, callLiteral(1)),
-        callLiteral(1)
-      )
-
-    // or
+//import aqua.AquaSpec
+//import aqua.model.Node.{callLiteral, on, par}
+//import aqua.model.transform.BodyConfig
+//import aqua.model.{AquaContext, LiteralModel, Node}
+//import aqua.parser.Ast
+//import aqua.parser.lift.{LiftParser, Span}
+//import aqua.types.LiteralType
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.should.Matchers
+//
+//class SemanticsSpec extends AnyFlatSpec with Matchers with AquaSpec {
+//
+//  // use it to fix https://github.com/fluencelabs/aqua/issues/90
+//  ignore should "create right model" in {
+//    implicit val fileLift: LiftParser[Span.F] = Span.spanLiftParser
+//
+//    val script =
+//      """service LocalPrint("srv1"):
+//        |    fn1: -> ()
+//        |
+//        |func topologyTest():
+//        |    on "other-peer":
+//        |        LocalPrint.fn1()
+//        |    par LocalPrint.fn1()
+//        |""".stripMargin
+//
+//    val ast = Ast.fromString(script).toList.head
+//    val ctx = AquaContext.blank
+//    val bc = BodyConfig()
+//    import bc.aquaContextMonoid
+//
+//    val context = Semantics.process(ast, ctx).toList.head
+//
+//    val body = context.allFuncs()("topologyTest").body.tree.forceAll
+//
+//    val node = Node.cofToNode(body)
+//
 //    val expected =
-//      seq(par(on(LiteralModel("\"other-peer\"", LiteralType.string), Nil, callL(1)), callL(1)))
-
-    node should be(expected)
-  }
-}
+//      par(
+//        on(LiteralModel("\"other-peer\"", LiteralType.string), Nil, callLiteral(1)),
+//        callLiteral(1)
+//      )
+//
+//    // or
+////    val expected =
+////      seq(par(on(LiteralModel("\"other-peer\"", LiteralType.string), Nil, callL(1)), callL(1)))
+//
+//    node should be(expected)
+//  }
+//}
