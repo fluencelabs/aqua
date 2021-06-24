9d9e4100c062ceffae357a304b916ecf3d90a35b
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-06-24 17:58:01 +0300

    co test

---
+++ parser/src/test/scala/aqua/parser/CoExprSpec.scala
@@ -0,0 +1,35 @@
+package aqua.parser
+
+import aqua.AquaSpec
+import aqua.parser.expr.{CallArrowExpr, CoExpr}
+import aqua.parser.lexer.Token
+import aqua.parser.lift.LiftParser.Implicits.idLiftParser
+import cats.data.Chain
+import cats.free.Cofree
+import cats.{Eval, Id}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class CoExprSpec extends AnyFlatSpec with Matchers with AquaSpec {
+
+  "co" should "be parsed" in {
+    CoExpr.readLine[Id].parseAll("co x <- y()").value should be(
+      Cofree[Chain, Expr[Id]](
+        CoExpr[Id](Token.lift[Id, Unit](())),
+        Eval.now(
+          Chain(
+            Cofree[Chain, Expr[Id]](
+              CallArrowExpr(
+                Some(AquaSpec.toName("x")),
+                None,
+                AquaSpec.toName("y"),
+                Nil
+              ),
+              Eval.now(Chain.empty)
+            )
+          )
+        )
+      )
+    )
+  }
+}
