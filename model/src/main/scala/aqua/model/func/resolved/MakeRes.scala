9917591fff4eef4eafc1a20eb0876e373478b506
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-07-13 14:44:50 +0300

    simple topology refactoring

--- model/src/main/scala/aqua/model/func/resolved/MakeRes.scala
+++ model/src/main/scala/aqua/model/func/resolved/MakeRes.scala
@@ -1,6 +1,7 @@
 package aqua.model.func.resolved
 
 import aqua.model.func.Call
+import aqua.model.topology.Topology.Res
 import aqua.model.{LiteralModel, ValueModel}
 import cats.Eval
 import cats.data.Chain
@@ -8,24 +9,24 @@ import cats.free.Cofree
 
 object MakeRes {
   val nilTail: Eval[Chain[Cofree[Chain, ResolvedOp]]] = Eval.now(Chain.empty)
-  type Cof = Cofree[Chain, ResolvedOp]
-  def leaf(op: ResolvedOp): Cof = Cofree[Chain, ResolvedOp](op, nilTail)
 
-  def next(item: String): Cof =
+  def leaf(op: ResolvedOp): Res = Cofree[Chain, ResolvedOp](op, nilTail)
+
+  def next(item: String): Res =
     leaf(NextRes(item))
 
-  def seq(first: Cof, second: Cof, more: Cof*): Cof =
+  def seq(first: Res, second: Res, more: Res*): Res =
     Cofree[Chain, ResolvedOp](SeqRes, Eval.later(first +: second +: Chain.fromSeq(more)))
 
-  def par(first: Cof, second: Cof, more: Cof*): Cof =
+  def par(first: Res, second: Res, more: Res*): Res =
     Cofree[Chain, ResolvedOp](ParRes, Eval.later(first +: second +: Chain.fromSeq(more)))
 
-  def xor(first: Cof, second: Cof): Cof =
+  def xor(first: Res, second: Res): Res =
     Cofree[Chain, ResolvedOp](XorRes, Eval.later(Chain(first, second)))
 
-  def fold(item: String, iter: ValueModel, body: Cof): Cof =
+  def fold(item: String, iter: ValueModel, body: Res): Res =
     Cofree[Chain, ResolvedOp](FoldRes(item, iter), Eval.now(Chain.one(body)))
 
-  def noop(onPeer: ValueModel): Cof =
+  def noop(onPeer: ValueModel): Res =
     leaf(CallServiceRes(LiteralModel.quote("op"), "noop", Call(Nil, None), onPeer))
 }
