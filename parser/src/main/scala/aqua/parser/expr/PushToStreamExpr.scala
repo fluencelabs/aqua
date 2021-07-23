6fceaf7bc8ecada4f65df19189cd00fdb1319d59
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-07-23 16:48:19 +0300

    push-to-stream type checking

--- parser/src/main/scala/aqua/parser/expr/PushToStreamExpr.scala
+++ parser/src/main/scala/aqua/parser/expr/PushToStreamExpr.scala
@@ -2,20 +2,20 @@ package aqua.parser.expr
 
 import aqua.parser.Expr
 import aqua.parser.lexer.Token._
-import aqua.parser.lexer.{Name, Value}
+import aqua.parser.lexer.{Value, VarLambda}
 import aqua.parser.lift.LiftParser
 import cats.Comonad
 import cats.parse.{Parser => P}
 
 case class PushToStreamExpr[F[_]](
-  variable: Name[F],
+  stream: VarLambda[F],
   value: Value[F]
-) extends Expr[F](PushToStreamExpr, variable)
+) extends Expr[F](PushToStreamExpr, stream)
 
 object PushToStreamExpr extends Expr.Leaf {
 
   override def p[F[_]: LiftParser: Comonad]: P[PushToStreamExpr[F]] =
-    ((Name.p[F] <* ` <<- `).with1 ~ Value.`value`).map { case (variable, value) =>
+    ((Value.varLambda[F] <* ` <<- `).with1 ~ Value.`value`).map { case (variable, value) =>
       PushToStreamExpr(variable, value)
     }
 }
--- semantics/src/main/scala/aqua/semantics/expr/PushToStreamSem.scala
+++ semantics/src/main/scala/aqua/semantics/expr/PushToStreamSem.scala
@@ -7,25 +7,34 @@ import aqua.parser.expr.PushToStreamExpr
 import aqua.semantics.Prog
 import aqua.semantics.rules.ValuesAlgebra
 import aqua.semantics.rules.names.NamesAlgebra
+import aqua.semantics.rules.types.TypesAlgebra
 import cats.free.Free
-import cats.syntax.functor._
 
 class PushToStreamSem[F[_]](val expr: PushToStreamExpr[F]) extends AnyVal {
 
   def program[Alg[_]](implicit
     N: NamesAlgebra[F, Alg],
+    T: TypesAlgebra[F, Alg],
     V: ValuesAlgebra[F, Alg]
   ): Prog[Alg, Model] =
     V.valueToModel(expr.value).flatMap {
       case Some(vm) =>
-        N.read(expr.variable) as (FuncOp
-          .leaf(
-            CallServiceTag(
-              LiteralModel.quote("op"),
-              "identity",
-              Call(vm :: Nil, Some(Call.Export(expr.variable.value, vm.`type`)))
-            )
-          ): Model)
+        for {
+          resolvedStreamType <- N.read(expr.stream.name).map(_.get)
+          valueType <- V.resolveType(expr.value)
+          _ <- T.expectStream(expr.stream, resolvedStreamType)
+          _ <- T.expectStreamMember(expr.value, resolvedStreamType, valueType)
+        } yield {
+          FuncOp
+            .leaf(
+              CallServiceTag(
+                LiteralModel.quote("op"),
+                "identity",
+                Call(vm :: Nil, Some(Call.Export(expr.stream.name.value, resolvedStreamType)))
+              )
+            ): Model
+        }
+
       case _ => Free.pure[Alg, Model](Model.error("Cannot resolve stream type"))
     }
 
