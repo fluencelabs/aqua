bc461457da303b4a90a48d7a0926762c38be59ca
Dima <dmitry.shakhtarin@fluence.ai>
2021-08-31 12:20:22 +0300

    Add JS build to package and to integration tests (#256)

---
+++ npm/index-experimental.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require('fs');
+
+let importArgs = []
+
+const nm = path.join("./", "node_modules")
+if (fs.existsSync(nm) && fs.lstatSync(nm).isDirectory()) {
+  importArgs = ["-m", "node_modules"]
+}
+
+const args = [
+  "node",
+  path.join(__dirname, "aqua-cli-experimental.js"),
+    ...importArgs,
+  ...process.argv.slice(2),
+];
+
+const argsString = args.join(" ");
+
+console.log("Aqua JS: " + argsString);
+exec(argsString, (err, stdout, stderr) => {
+  console.error("Aqua JS: " + stderr);
+  console.log("Aqua JS: " + stdout);
+
+  if (err) {
+    process.exit(err.code);
+  }
+});
