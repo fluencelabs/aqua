1bf7e3fdd50fac9e7f46ec4904d4b1df13b870de
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-09-16 15:38:32 +0300

    fix import

--- semantics/src/main/scala/aqua/semantics/Levenshtein.scala
+++ semantics/src/main/scala/aqua/semantics/Levenshtein.scala
@@ -1,8 +1,8 @@
 package aqua.semantics
 
-import scala.collection.mutable
-import scala.collection.mutable
 import cats.data.{Chain, NonEmptyList}
+
+import scala.collection.mutable
 
 object Levenshtein {
 
@@ -10,17 +10,21 @@ object Levenshtein {
   def levenshtein(s1: String, s2: String): Int = {
     val memoizedCosts = mutable.Map[(Int, Int), Int]()
 
-    def lev: ((Int, Int)) => Int = {
-      case (k1, k2) =>
-        memoizedCosts.getOrElseUpdate((k1, k2), (k1, k2) match {
+    def lev: ((Int, Int)) => Int = { case (k1, k2) =>
+      memoizedCosts.getOrElseUpdate(
+        (k1, k2),
+        (k1, k2) match {
           case (i, 0) => i
           case (0, j) => j
           case (i, j) =>
-            Seq(1 + lev((i - 1, j)),
+            Seq(
+              1 + lev((i - 1, j)),
               1 + lev((i, j - 1)),
               lev((i - 1, j - 1))
-                + (if (s1(i - 1) != s2(j - 1)) 1 else 0)).min
-        })
+                + (if (s1(i - 1) != s2(j - 1)) 1 else 0)
+            ).min
+        }
+      )
     }
 
     lev((s1.length, s2.length))
