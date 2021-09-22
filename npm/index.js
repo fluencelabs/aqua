f27ae5eda9f7ac8bcc2089a081ccdafc9350b6d5
Dima <dmitry.shakhtarin@fluence.ai>
2021-09-22 12:38:47 +0300

    Fix version flag (#313)

--- npm/index.js
+++ npm/index.js
@@ -6,26 +6,35 @@ const { exec } = require("child_process");
 const path = require("path");
 const fs = require('fs');
 
-let importArgs = []
-
 const nm = path.join("./", "node_modules")
-if (fs.existsSync(nm) && fs.lstatSync(nm).isDirectory()) {
-  importArgs = ["-m", "node_modules"]
-}
+let initArgs = process.argv.slice(2)
 
-const args = [
-  "node",
-  path.join(__dirname, "aqua.js"),
-    ...importArgs,
-  ...process.argv.slice(2),
-];
+let args = [];
+if ((initArgs.includes('-v') || initArgs.includes('--version'))) {
+    args = [
+      "node",
+      path.join(__dirname, "aqua.js"),
+      "--version",
+    ];
+} else {
+    let importArgs = []
+    if (fs.existsSync(nm) && fs.lstatSync(nm).isDirectory()) {
+      importArgs = ["-m", "node_modules"]
+    }
+    args = [
+      "node",
+      path.join(__dirname, "aqua.js"),
+      ...importArgs,
+      ...initArgs,
+    ];
+}
 
 const argsString = args.join(" ");
 
-console.log("Aqua JS: " + argsString);
+console.log("Aqua: " + argsString);
 exec(argsString, (err, stdout, stderr) => {
-  console.error("Aqua JS: " + stderr);
-  console.log("Aqua JS: " + stdout);
+  console.error("Aqua: " + stderr);
+  console.log("Aqua: " + stdout);
 
   if (err) {
     process.exit(err.code);
