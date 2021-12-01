60c90fbd1dcad002f4bea24b4cc54af5b47d2c8d
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-12-01 15:46:26 +0300

    fix compilation

--- cli/.jvm/src/main/scala/aqua/PlatformUtils.scala
+++ cli/.jvm/src/main/scala/aqua/PlatformUtils.scala
@@ -1,7 +1,5 @@
 package aqua
 
-import aqua.Utils.logger
-import aqua.js.{Meta, Module}
 import fs2.io.file.Path
 import scribe.Logging
 
@@ -9,6 +7,6 @@ import scala.util.Try
 
 // Java-specific functions
 object PlatformUtils extends Logging {
-  // you need to add import with builtin.aqua manually when running in JVM environment 
+  // you need to add import with builtin.aqua manually when running in JVM environment
   def getBuiltinNodeModulePaths: Option[Path] = None
 }
