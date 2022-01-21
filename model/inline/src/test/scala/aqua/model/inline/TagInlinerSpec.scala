5bc1ba510e27e4898956751844a46a6dab6b0283
alari <dmitry@fluence.one>
2022-01-21 14:34:22 +0300

    Failing test in TagInlinerSpec

--- model/inline/src/test/scala/aqua/model/inline/TagInlinerSpec.scala
+++ model/inline/src/test/scala/aqua/model/inline/TagInlinerSpec.scala
@@ -1,8 +1,8 @@
 package aqua.model.inline
 
-import aqua.model.{IntoIndexModel, VarModel}
+import aqua.model.{FlattenModel, IntoIndexModel, VarModel}
 import aqua.model.inline.state.InliningState
-import aqua.raw.value.{IntoIndexRaw, VarRaw}
+import aqua.raw.value.{IntoIndexRaw, LiteralRaw, VarRaw}
 import aqua.types.*
 import cats.data.NonEmptyMap
 import cats.data.Chain
@@ -33,6 +33,44 @@ class TagInlinerSpec extends AnyFlatSpec with Matchers {
         ArrayType(ScalarType.string),
         Chain.one(IntoIndexModel("y", ScalarType.string))
       ) -> None
+    )
+  }
+
+  "tag inliner" should "desugarize a single recursive raw value" in {
+    TagInliner
+      .desugarize[InliningState](
+        VarRaw(
+          "x",
+          ArrayType(ScalarType.string),
+          Chain.one(
+            IntoIndexRaw(
+              VarRaw(
+                "ys",
+                ArrayType(ScalarType.i8),
+                Chain.one(IntoIndexRaw(LiteralRaw.number(0), LiteralType.number))
+              ),
+              ScalarType.string
+            )
+          )
+        )
+      )
+      .run(InliningState())
+      .value
+      ._2 should be(
+      VarModel(
+        "x",
+        ArrayType(ScalarType.string),
+        Chain.one(IntoIndexModel("ys-1", ScalarType.string))
+      ) -> Some(
+        FlattenModel(
+          VarModel(
+            "ys",
+            ArrayType(ScalarType.i8),
+            Chain.one(IntoIndexModel("0", LiteralType.number))
+          ),
+          "ys-1"
+        ).leaf
+      )
     )
   }
 
