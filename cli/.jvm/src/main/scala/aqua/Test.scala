5081f0af32e4b04090c08239d7665d26f0de551a
alari <dmitry@fluence.one>
2022-01-25 19:07:08 +0300

    foldJoin reproduces the bug

--- cli/.jvm/src/main/scala/aqua/Test.scala
+++ cli/.jvm/src/main/scala/aqua/Test.scala
@@ -22,7 +22,7 @@ object Test extends IOApp.Simple {
       start <- IO(System.currentTimeMillis())
       _ <- AquaPathCompiler
         .compileFilesTo[IO](
-          Path("./aqua-src/test/wrong-par.aqua"),
+          Path("./aqua-src/foldJoin.aqua"),
           List(Path("./aqua")),
           Option(Path("./target")),
           TypeScriptBackend,
