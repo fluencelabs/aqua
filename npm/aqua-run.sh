de0a42d8e14bb58b46435f8270906cee85aa1ac0
DieMyst <dmitry.shakhtarin@fluence.ai>
2022-02-02 17:44:27 +0300

    debug things

--- npm/aqua-run.sh
+++ npm/aqua-run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-FUNC='identityArgsAndReturn(structField, stringField, numberField)'
-INPUT='test/sample.aqua'
+FUNC='something(node)'
+INPUT='../aqua-src/test.aqua'
 DATAPATH='test/data.json'
 ADDR='/dns4/kras-04.fluence.dev/tcp/19001/wss/p2p/12D3KooWFEwNWcHqi9rtsmDhsYcDbRUCDXH84RC4FW6UfsFWaoHi'
 # IMPORT=
 
 if [ -z "$IMPORT" ]
 then
-      npm run from:scalajs -- run -f "$FUNC" -i "$INPUT" --data-path "$DATAPATH" --addr "$ADDR"
+      npm run from:scalajs -- run -f "$FUNC" -i "$INPUT" --data-path "$DATAPATH" --addr "$ADDR" --print-air
 else
       npm run from:scalajs -- run -f "$FUNC" -i "$INPUT" --data-path "$DATAPATH" --addr "$ADDR" -m "$IMPORT"
 fi
