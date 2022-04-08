cafd19c5d49684e15b42ab1159bfe04469408fc5
DieMyst <dmitry.shakhtarin@fluence.ai>
2022-04-08 11:28:06 +0300

    more unit tests

--- parser/src/test/scala/aqua/parser/InfixTokenSpec.scala
+++ parser/src/test/scala/aqua/parser/InfixTokenSpec.scala
@@ -2,6 +2,7 @@ package aqua.parser
 
 import aqua.AquaSpec
 import aqua.parser.expr.func.IfExpr
+import aqua.parser.lexer.InfixToken.Op
 import aqua.parser.lexer.{BracketsToken, EqOp, InfixToken, LiteralToken, ValueToken}
 import aqua.parser.lexer.InfixToken.Op.*
 import aqua.parser.lift.Span
@@ -21,27 +22,62 @@ class InfixTokenSpec extends AnyFlatSpec with Matchers with AquaSpec {
     }
   }
 
-  private def literal(n: Int) = LiteralToken[Id](n.toString, LiteralType.number)
-
   import AquaSpec._
+
+  private def literal(n: Int): ValueToken[Id] = LiteralToken[Id](n.toString, LiteralType.number)
+
+  private def infixToken(left: ValueToken[Id], right: ValueToken[Id], op: Op) =
+    InfixToken[Id](left, right, op)
+
+  private def mul(left: ValueToken[Id], right: ValueToken[Id]): ValueToken[Id] =
+    infixToken(left, right, Mul)
+
+  private def sub(left: ValueToken[Id], right: ValueToken[Id]): ValueToken[Id] =
+    infixToken(left, right, Sub)
+
+  private def div(left: ValueToken[Id], right: ValueToken[Id]): ValueToken[Id] =
+    infixToken(left, right, Div)
+
+  private def rem(left: ValueToken[Id], right: ValueToken[Id]): ValueToken[Id] =
+    infixToken(left, right, Rem)
+
+  private def add(left: ValueToken[Id], right: ValueToken[Id]): ValueToken[Id] =
+    infixToken(left, right, Add)
+
+  private def pow(left: ValueToken[Id], right: ValueToken[Id]): ValueToken[Id] =
+    infixToken(left, right, Pow)
 
   "primitive math expression" should "be parfvfsed" in {
 
     val vt = ValueToken.atom.parseAll("3").right.get.mapK(spanToId)
     val vt2 = InfixToken.mult.parseAll("3 * 2 * 5").right.get.mapK(spanToId)
 
-    val vt31 = ValueToken.`_value`.parseAll("3 + 1 * 2 - 2").right.get.mapK(spanToId)
-    val vt32 = ValueToken.`_value`.parseAll("5 + 6 + 10 + 20 + 1 + 2 + 4").right.get.mapK(spanToId)
-    val vt4 = ValueToken.`_value`.parseAll("3 * 2 * 5 + 3").right.get.mapK(spanToId)
-    val vt5 = ValueToken.`_value`.parseAll("2 + 3 * 2 - 3 * 5 + 2").right.get.mapK(spanToId)
-    val vt6 = ValueToken.`_value`.parseAll("2 * 3 / 5 * 4").right.get.mapK(spanToId)
-    val vt7 = ValueToken.`_value`
-      .parseAll("3 - 2 + 3 * 3 / 5 * 2 - 4 / 4 ** 2 ** 2 % 2 + 1 - 4 > 2 * 3 - 5 + 1 * 2 / 2")
+    val vt3 = ValueToken.`_value`.parseAll("3 + 1 * 2 - 2").right.get.mapK(spanToId)
+    val vt4 = ValueToken.`_value`.parseAll("5 + 6 + 10 + 20 + 1 + 2 + 4").right.get.mapK(spanToId)
+    val vt5 = ValueToken.`_value`.parseAll("3 * 2 * 5 + 3").right.get.mapK(spanToId)
+    val vt6 = ValueToken.`_value`.parseAll("2 + 3 * 2 - 3 * 5 + 2").right.get.mapK(spanToId)
+    val vt7 = ValueToken.`_value`.parseAll("2 * 3 / 5 * 4").right.get.mapK(spanToId)
     val vt8 = ValueToken.`_value`
+      .parseAll("3 - 2 + 3 * 3 / 5 * 2 - 4 / 4 ** 2 ** 2 % 2 + 1 - 4 > 2 * 3 - 5 + 1 * 2 / 2")
+      .right
+      .get
+      .mapK(spanToId)
+    val vt9 = ValueToken.`_value`
       .parseAll("5 > 4")
+      .right
+      .get
+      .mapK(spanToId)
 
-    println(vt7)
-    println(vt8)
+    vt shouldBe literal(3)
+    vt2 shouldBe mul(mul(3, 2), 5)
+    vt3 shouldBe sub(add(3, mul(1, 2)), 2)
+    vt4 shouldBe add(add(add(add(add(add(5, 6), 10), 20), 1), 2), 4)
+    vt5 shouldBe add(mul(mul(3, 2), 5), 3)
+    vt6 shouldBe add(sub(add(2, mul(3, 2)), mul(3, 5)), 2)
+    vt7 shouldBe mul(div(mul(2, 3), 5), 4)
+
+    // todo: add after pow will be right-associative
+    // vt8 shouldBe
   }
 
   "primitive math expression" should "be parsed" in {
