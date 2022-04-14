e3957c129659ef55ad89d91a88519e2eb5f2f516
Dima <dmitry.shakhtarin@fluence.ai>
2022-04-14 15:04:43 +0300

    handle EPIPE error properly (#491)

--- npm/index.js
+++ npm/index.js
@@ -2,10 +2,31 @@
 
 "use strict";
 
-process.stdout.on('error', function( err ) {
-    if (err.code == "EPIPE") {
-        process.exit(0);
+handleEPIPE(process.stderr)
+handleEPIPE(process.stdout)
+function handleEPIPE(stream) {
+  stream.on('error', onerror)
+  function onerror(err) {
+    if (err.code === 'EPIPE') {
+      stream._write = noopWrite
+      stream._writev = noopWritev
+      stream._read = noopRead
+      return stream.removeListener('error', onerror)
     }
-});
+    if (EE.listenerCount(stream, 'error') === 1) {
+      stream.removeListener('error', onerror)
+      stream.emit('error', err)
+    }
+  }
+}
+function noopWrite(chunk, enc, cb) {
+  cb()
+}
+function noopRead() {
+  this.push('')
+}
+function noopWritev(chunks, cb) {
+  cb()
+}
 
 import "./aqua.js";
