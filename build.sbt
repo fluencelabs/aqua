e409605d216c708015a78346801aa48ba9bdd7ee
Dima <dmitry.shakhtarin@fluence.ai>
2022-09-20 18:04:55 +0400

    LNG-85 Streams restrictions in aqua (#555)

--- build.sbt
+++ build.sbt
@@ -17,7 +17,7 @@ val scribeV = "3.7.1"
 name := "aqua-hll"
 
 val commons = Seq(
-  baseAquaVersion := "0.7.5",
+  baseAquaVersion := "0.7.6",
   version         := baseAquaVersion.value + "-" + sys.env.getOrElse("BUILD_NUMBER", "SNAPSHOT"),
   scalaVersion    := dottyVersion,
   libraryDependencies ++= Seq(
--- parser/src/main/scala/aqua/parser/expr/func/AssignmentExpr.scala
+++ parser/src/main/scala/aqua/parser/expr/func/AssignmentExpr.scala
@@ -3,10 +3,10 @@ package aqua.parser.expr.func
 import aqua.parser.Expr
 import aqua.parser.expr.func.AssignmentExpr
 import aqua.parser.lexer.Token.*
-import aqua.parser.lexer.{Name, ValueToken}
+import aqua.parser.lexer.{CollectionToken, Name, ValueToken}
 import aqua.parser.lift.LiftParser
 import cats.parse.Parser as P
-import cats.{Comonad, ~>}
+import cats.{~>, Comonad}
 import aqua.parser.lift.Span
 import aqua.parser.lift.Span.{P0ToSpan, PToSpan}
 
@@ -20,7 +20,16 @@ case class AssignmentExpr[F[_]](
 object AssignmentExpr extends Expr.Leaf {
 
   override val p: P[AssignmentExpr[Span.S]] =
-    ((Name.p <* ` = `).with1 ~ ValueToken.`value`).map { case (variable, value) =>
-      AssignmentExpr(variable, value)
+    ((Name.p <* ` = `).with1 ~ ValueToken.`value`).flatMap { case (variable, value) =>
+      value match {
+        case CollectionToken(_, values) =>
+          if (values.isEmpty)
+            P.failWith(
+              "Assigning empty array to a variable is prohibited. You can create an array with values (like '[a, b, c]') or use '[]' in place."
+            )
+          else P.pure(AssignmentExpr(variable, value))
+        case _ =>
+          P.pure(AssignmentExpr(variable, value))
+      }
     }
 }
