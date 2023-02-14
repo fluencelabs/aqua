e4a2b7534dca4d522ea19810e03035e65e97a784
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-02-14 09:10:28 +0400

    Aqua API example

--- api/aqua-api-example/index.js
+++ api/aqua-api-example/index.js
@@ -3,8 +3,6 @@ import {
     Call,
     Path,
 } from "@fluencelabs/aqua-api/aqua-api.js";
-import { FluencePeer } from "@fluencelabs/fluence";
-import { callFunctionImpl } from "@fluencelabs/fluence/dist/internal/compilerSupport/v3impl/callFunction.js";
 
 const aquaPath = new Path("test.aqua")
 // write function that we want to call and arguments
@@ -14,19 +12,21 @@ const call = new Call("getNumber(num)", args, aquaPath)
 // compile call
 const compilationResult = await Aqua.compile(call, [])
 
+
+/*
+// Compilation result definition
+export class CompilationResult {
+    // List of service definitions to register in Fluence JS Client
+    services: Record<string, ServiceDef>
+    // List of function definitions to call in Fluence JS Client
+    functions: Record<string, AquaFunction>
+    // Definition of wrapped function to call in Fluence JS Client
+    functionCall?: AquaFunction
+    // List of errors. All other fields will be empty if `errors` not empty
+    errors: string[]
+}
+ */
+
 // get function definition, that describes types of arguments and results of a function
 // and AIR script
 const {funcDef, script} = compilationResult.functionCall
-
-// initialize fluence client
-const fluence = new FluencePeer();
-await fluence.start({})
-
-// make a call with arguments
-const result = await callFunctionImpl(funcDef, script, {}, fluence, args)
-
-// print a result
-console.log(result)
-
-// stop fluence client
-await fluence.stop()
