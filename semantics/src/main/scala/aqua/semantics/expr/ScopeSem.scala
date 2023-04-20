561d3e182087c38bc61e7b86111cb4a55226450b
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-04-20 13:23:40 +0400

    delete comments

--- semantics/src/main/scala/aqua/semantics/expr/ScopeSem.scala
+++ semantics/src/main/scala/aqua/semantics/expr/ScopeSem.scala
@@ -28,46 +28,5 @@ class ScopeSem[S[_]](val expr: ScopeExpr[S]) extends AnyVal {
     Prog.around(
       A.beginScope(expr.name),
       (_: Unit, _: Raw) =>
-        Raw.error("Scope has no arrows and fields, fails").pure[Alg]
-        /*(A.purgeArrows(expr.name), T.purgeFields(expr.name.value, expr.name)).flatMapN {
-          case (None, None) =>
-            Raw.error("Scope has no arrows and fields, fails").pure[Alg]
-          case (Some(arrows), Some(fields)) =>
-            A.defineScope(expr.name, fields) as (ScopeRaw(expr.name.value, fields): Raw)
-          case (None, Some(fields)) => A.defineScope(expr.name, fields) as (ScopeRaw(expr.name.value, fields): Raw)
-//          case (Some(arrows), None) => A.defineScope(expr.name, arrows.toNem.mapKeys(_.name)) as (ScopeRaw(expr.name.value, fields): Raw)
-          case (_, _) =>
-            Raw.error("Scope has no arrows and fields, fails").pure[Alg]
-
-        } <* A.endScope()*/
-    )
-
-  /*private def defineArrows[Alg[_]: Monad](arrowsOp: Option[NonEmptyList[(Name[S], ArrowType)]])(
-    implicit
-    A: AbilitiesAlgebra[S, Alg],
-    N: NamesAlgebra[S, Alg],
-    T: TypesAlgebra[S, Alg],
-    V: ValuesAlgebra[S, Alg]
-  ) = arrowsOp match {
-    case Some(nel) =>
-      val arrows = nel.map(kv => kv._1.value -> (kv._1, kv._2)).toNem
-      for {
-        defineResult <- A.defineScope(
-          expr.name,
-          arrows
-        )
-      } yield
-        if (defineResult) {
-          ScopeRaw(expr.name.value, arrows.map(_._2), defaultId)
-        } else Raw.empty("Scope not created due to validation errors")
-    case None =>
-
-  }
-
-  private def defineFields[Alg[_]: Monad](arrows: Option[NonEmptyList[(String, Type)]])(implicit
-    A: AbilitiesAlgebra[S, Alg],
-    N: NamesAlgebra[S, Alg],
-    T: TypesAlgebra[S, Alg],
-    V: ValuesAlgebra[S, Alg]
-  ) = {}*/
+        Raw.error("Undefined").pure[Alg])
 }
