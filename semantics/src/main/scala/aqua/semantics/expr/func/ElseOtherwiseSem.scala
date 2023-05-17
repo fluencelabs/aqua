0b4d9e57106d482eab95f7f565aaebc5020f7800
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-05-17 22:21:29 +0900

    add name scopes to if/else/try

--- semantics/src/main/scala/aqua/semantics/expr/func/ElseOtherwiseSem.scala
+++ semantics/src/main/scala/aqua/semantics/expr/func/ElseOtherwiseSem.scala
@@ -5,16 +5,24 @@ import aqua.parser.expr.func.ElseOtherwiseExpr
 import aqua.raw.Raw
 import aqua.semantics.Prog
 import aqua.semantics.rules.abilities.AbilitiesAlgebra
+import aqua.semantics.rules.locations.LocationsAlgebra
+import aqua.semantics.rules.names.NamesAlgebra
 import cats.syntax.applicative.*
 import cats.Monad
 
 class ElseOtherwiseSem[S[_]](val expr: ElseOtherwiseExpr[S]) extends AnyVal {
 
-  def program[Alg[_]: Monad](implicit A: AbilitiesAlgebra[S, Alg]): Prog[Alg, Raw] =
+  def program[Alg[_]: Monad](implicit
+    A: AbilitiesAlgebra[S, Alg],
+    N: NamesAlgebra[S, Alg],
+    L: LocationsAlgebra[S, Alg]
+  ): Prog[Alg, Raw] =
     Prog
       .after[Alg, Raw] {
         case FuncOp(g) => XorTag.wrap(g).toFuncOp.pure[Alg]
         case g => g.pure[Alg]
       }
       .abilitiesScope(expr.token)
+      .namesScope(expr.token)
+      .locationsScope()
 }
