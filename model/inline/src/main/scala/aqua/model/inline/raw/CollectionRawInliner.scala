676094c2a5d74e8a1bc72a6dbd16cb814869edc7
Dima <dmitry.shakhtarin@fluence.ai>
2023-06-01 16:43:19 +0300

    Update model/inline/src/main/scala/aqua/model/inline/raw/CollectionRawInliner.scala
    Co-authored-by: InversionSpaces <InversionSpaces@vivaldi.net>

--- model/inline/src/main/scala/aqua/model/inline/raw/CollectionRawInliner.scala
+++ model/inline/src/main/scala/aqua/model/inline/raw/CollectionRawInliner.scala
@@ -66,7 +66,7 @@ object CollectionRawInliner extends RawInliner[CollectionRaw] {
         case OptionType(_) =>
           RestrictionModel(streamName, isStream = true).wrap(
             SeqModel.wrap(
-              XorModel.wrap(Chain(SeqModel.wrap(vals.toList:_*), NullModel.leaf).toList: _*),
+              XorModel.wrap(SeqModel.wrap(vals.toList:_*), NullModel.leaf),
               CanonicalizeModel(stream, canon).leaf
             )
           )
