97b9981a4e3704b0068b32638d42c9e86c76467b
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-06-05 16:25:33 +0200

    split types in API to input types and output

---
+++ api/aqua-api/.js/src/main/scala/api/types/InputTypes.scala
@@ -0,0 +1,72 @@
+package api.types
+
+import aqua.api.{AirType, AquaAPIConfig, JavaScriptType, TypeScriptType}
+import aqua.js.{FunctionDefJs, ServiceDefJs}
+import aqua.model.transform.TransformConfig
+import cats.data.Validated.{Invalid, Valid, invalidNec, validNec}
+import cats.data.{Chain, NonEmptyChain, Validated, ValidatedNec}
+
+import scala.scalajs.js
+import scala.scalajs.js.JSConverters.*
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import scala.scalajs.js.|
+
+@JSExportTopLevel("Input")
+class Input(
+  @JSExport
+  val input: String
+)
+
+@JSExportTopLevel("Path")
+class Path(
+  @JSExport
+  val path: String
+)
+
+@JSExportTopLevel("Call")
+class Call(
+  @JSExport
+  val functionCall: String,
+  @JSExport
+  val arguments: js.Dynamic,
+  @JSExport
+  val input: Path | Input
+)
+
+@JSExportTopLevel("AquaConfig")
+class AquaConfig(
+  @JSExport
+  val logLevel: js.UndefOr[String],
+  @JSExport
+  val constants: js.UndefOr[js.Array[String]],
+  @JSExport
+  val noXor: js.UndefOr[Boolean],
+  @JSExport
+  val noRelay: js.UndefOr[Boolean],
+  @JSExport
+  val targetType: js.UndefOr[String]
+)
+
+object AquaConfig {
+
+  def fromJS(cjs: AquaConfig): ValidatedNec[String, AquaAPIConfig] = {
+    cjs.targetType.toOption
+      .map(_.toLowerCase())
+      .map {
+        case "typescript" => validNec(TypeScriptType)
+        case "javascript" => validNec(JavaScriptType)
+        case "air" => validNec(AirType)
+        case _ => invalidNec("Target can be only 'typescript', 'javascript', or 'air'")
+      }
+      .getOrElse(validNec(AirType))
+      .map { target =>
+        AquaAPIConfig(
+          target,
+          cjs.logLevel.getOrElse("info"),
+          cjs.constants.map(_.toList).getOrElse(Nil),
+          cjs.noXor.getOrElse(false),
+          cjs.noRelay.getOrElse(false)
+        )
+      }
+  }
+}
--- api/aqua-api/.js/src/main/scala/api/types/OutputTypes.scala
+++ api/aqua-api/.js/src/main/scala/api/types/OutputTypes.scala
@@ -1,15 +1,14 @@
 package api.types
 
-import aqua.api.{AirType, AquaAPIConfig, JavaScriptType, TypeScriptType}
 import aqua.js.{FunctionDefJs, ServiceDefJs}
 import aqua.model.transform.TransformConfig
+import cats.data.Validated.{Invalid, Valid, invalidNec, validNec}
 import cats.data.{Chain, NonEmptyChain, Validated, ValidatedNec}
-import cats.data.Validated.{invalidNec, validNec, Invalid, Valid}
 
 import scala.scalajs.js
+import scala.scalajs.js.JSConverters.*
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 import scala.scalajs.js.|
-import scala.scalajs.js.JSConverters.*
 
 @JSExportTopLevel("AquaFunction")
 case class AquaFunction(
@@ -35,42 +34,6 @@ object GeneratedSource {
   def tsSource(name: String, tsSource: String) = new GeneratedSource(name, tsSource, null, null)
   def jsSource(name: String, jsSource: String, tsTypes: String) = new GeneratedSource(name, null, jsSource, tsTypes)
 }
-
-@JSExportTopLevel("Input")
-class Input(
-  @JSExport
-  val input: String
-)
-
-@JSExportTopLevel("Path")
-class Path(
-  @JSExport
-  val path: String
-)
-
-@JSExportTopLevel("Call")
-class Call(
-  @JSExport
-  val functionCall: String,
-  @JSExport
-  val arguments: js.Dynamic,
-  @JSExport
-  val input: Path | Input
-)
-
-@JSExportTopLevel("AquaConfig")
-class AquaConfig(
-  @JSExport
-  val logLevel: js.UndefOr[String],
-  @JSExport
-  val constants: js.UndefOr[js.Array[String]],
-  @JSExport
-  val noXor: js.UndefOr[Boolean],
-  @JSExport
-  val noRelay: js.UndefOr[Boolean],
-  @JSExport
-  val targetType: js.UndefOr[String]
-)
 
 @JSExportTopLevel("CompilationResult")
 class CompilationResult(
@@ -100,28 +63,4 @@ object CompilationResult {
     errors: List[String]
   ): CompilationResult =
     CompilationResult(js.Dictionary(), js.Dictionary(), null, null, errors.toJSArray)
-}
-
-object AquaConfig {
-
-  def fromJS(cjs: AquaConfig): ValidatedNec[String, AquaAPIConfig] = {
-    cjs.targetType.toOption
-      .map(_.toLowerCase())
-      .map {
-        case "typescript" => validNec(TypeScriptType)
-        case "javascript" => validNec(JavaScriptType)
-        case "air" => validNec(AirType)
-        case _ => invalidNec("Target can be only 'typescript', 'javascript', or 'air'")
-      }
-      .getOrElse(validNec(AirType))
-      .map { target =>
-        AquaAPIConfig(
-          target,
-          cjs.logLevel.getOrElse("info"),
-          cjs.constants.map(_.toList).getOrElse(Nil),
-          cjs.noXor.getOrElse(false),
-          cjs.noRelay.getOrElse(false)
-        )
-      }
-  }
 }
