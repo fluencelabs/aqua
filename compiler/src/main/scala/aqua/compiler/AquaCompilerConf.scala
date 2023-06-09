876185839e0d3cc1df754e0af0f9ea27f5add4be
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-06-09 12:08:40 +0200

    revert

--- compiler/src/main/scala/aqua/compiler/AquaCompilerConf.scala
+++ compiler/src/main/scala/aqua/compiler/AquaCompilerConf.scala
@@ -5,8 +5,6 @@ import aqua.raw.ConstantRaw
 /**
  * What should compiler care about during compilation – before generator backend takes its role
  *
- * @param constants List of known constants
+ * @param constantsList List of known constants
  */
-case class AquaCompilerConf(constants: List[ConstantRaw] = Nil) {
-  val constantsList: List[ConstantRaw] = constants ++ ConstantRaw.defaultConstants(None)
-}
+case class AquaCompilerConf(constantsList: List[ConstantRaw] = Nil)
