8891dece03ea53c4e31579020cfd2b2823e88b1d
InversionSpaces <InversionSpaces@vivaldi.net>
2023-06-19 08:09:05 +0000

    Refactor For.beginsOn

--- model/transform/src/main/scala/aqua/model/transform/topology/strategy/For.scala
+++ model/transform/src/main/scala/aqua/model/transform/topology/strategy/For.scala
@@ -6,7 +6,12 @@ import aqua.model.{NextModel, OnModel}
 import cats.Eval
 import cats.syntax.apply.*
 import cats.data.Chain.*
+import cats.data.State
 import cats.syntax.option.*
+import cats.instances.list.*
+import cats.syntax.foldable.*
+import cats.syntax.applicative.*
+import cats.syntax.functor.*
 
 object For extends Begins {
   override def toString: String = "<for>"
@@ -17,21 +22,29 @@ object For extends Begins {
     // Skip `next` child because its `beginsOn` depends on `this.beginsOn`, see [bug LNG-149]
     (current.forModel zip firstNotNextChild(current).map(_.beginsOn)).map {
       case (model, childBeginsOn) =>
-        // Take path until this for's iterator is used
-        childBeginsOn.map(
-          _.reverse
-            .foldLeft((true, List.empty[OnModel])) {
-              case ((true, acc), OnModel(_, r)) if r.exists(_.usesVarNames.contains(model.item)) =>
-                (false, acc)
-              case ((true, acc @ (OnModel(_, r @ (r0 ==: _)) :: _)), OnModel(p, _))
-                  if p.usesVarNames.contains(model.item) =>
-                // This is to take the outstanding relay and force moving there
-                (false, OnModel(r0, r) :: acc)
-              case ((true, acc), on) => (true, on :: acc)
-              case ((false, acc), _) => (false, acc)
-            }
-            ._2
-        )
+        for {
+          child <- childBeginsOn
+          // Take path until this for's iterator is used
+          path <- child.reverse
+            .foldM(List.empty[OnModel])((acc, on) =>
+              State
+                .get[Boolean]
+                .flatMap(found =>
+                  if (found) acc.pure // Short circuit
+                  else
+                    (acc, on) match {
+                      case (_, OnModel(_, r)) if r.exists(_.usesVarNames.contains(model.item)) =>
+                        State.set(true).as(acc)
+                      case (OnModel(_, r @ (r0 ==: _)) :: _, OnModel(p, _))
+                          if p.usesVarNames.contains(model.item) =>
+                        // This is to take the outstanding relay and force moving there
+                        State.set(true).as(OnModel(r0, r) :: acc)
+                      case _ => (on :: acc).pure
+                    }
+                )
+            )
+            .runA(false)
+        } yield path
     } getOrElse super.beginsOn(current)
 
   /**
