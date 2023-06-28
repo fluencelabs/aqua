ae427181a93d721583162f3cc69aa4f2ff1de8fa
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-06-28 14:25:40 +0300

    ArrowInliner refactoring

--- model/inline/src/main/scala/aqua/model/inline/ArrowInliner.scala
+++ model/inline/src/main/scala/aqua/model/inline/ArrowInliner.scala
@@ -1,14 +1,13 @@
 package aqua.model.inline
 
-import aqua.model.inline.state.{Arrows, Counter, Exports, Mangler}
+import aqua.model
+import aqua.model.inline.state.{Arrows, Exports, Mangler}
 import aqua.model.*
 import aqua.raw.ops.RawTag
-import aqua.types.ArrowType
+import aqua.types.{ArrowType, BoxType, StreamType}
 import aqua.raw.value.{ValueRaw, VarRaw}
-import aqua.types.{BoxType, StreamType}
-import cats.data.{Chain, State, StateT}
-import cats.syntax.traverse.*
-import cats.syntax.show.*
+import cats.Eval
+import cats.data.{Chain, State}
 import scribe.Logging
 
 /**
@@ -24,68 +23,85 @@ object ArrowInliner extends Logging {
   ): State[S, OpModel.Tree] =
     callArrowRet(arrow, call).map(_._1)
 
+  private def getOutsideStreamNames[S: Exports]: State[S, Set[String]] =
+    Exports[S].exports
+      .map(exports =>
+        exports.collect { case e @ (n, VarModel(_, StreamType(_), _)) =>
+          n
+        }.toSet
+      )
+
+  // push results to streams if they are exported to streams
+  private def pushStreamResults[S: Mangler: Exports: Arrows](
+    outsideStreamNames: Set[String],
+    exportTo: List[CallModel.Export],
+    results: List[ValueRaw],
+    body: OpModel.Tree
+  ): State[S, (List[OpModel.Tree], List[ValueModel])] =
+    for {
+      // Fix return values with exports collected in the body
+      resolvedResult <- RawValueInliner.valueListToModel(results)
+    } yield {
+      // Fix the return values
+      val (ops, rets) = (exportTo zip resolvedResult)
+        .map[(List[OpModel.Tree], ValueModel)] {
+          case (
+                CallModel.Export(n, StreamType(_)),
+                (res @ VarModel(_, StreamType(_), _), resDesugar)
+              ) if !outsideStreamNames.contains(n) =>
+            resDesugar.toList -> res
+          case (CallModel.Export(exp, st @ StreamType(_)), (res, resDesugar)) =>
+            // pass nested function results to a stream
+            (resDesugar.toList :+ PushToStreamModel(
+              res,
+              CallModel.Export(exp, st)
+            ).leaf) -> VarModel(
+              exp,
+              st,
+              Chain.empty
+            )
+          case (_, (res, resDesugar)) =>
+            resDesugar.toList -> res
+        }
+        .foldLeft[(List[OpModel.Tree], List[ValueModel])](
+          (body :: Nil, Nil)
+        ) { case ((ops, rets), (fo, r)) =>
+          (fo ::: ops, r :: rets)
+        }
+
+      (ops, rets)
+    }
+
   // Apply a callable function, get its fully resolved body & optional value, if any
   private def inline[S: Mangler: Arrows: Exports](
     fn: FuncArrow,
     call: CallModel
   ): State[S, (OpModel.Tree, List[ValueModel])] =
-    Exports[S].exports
-      .map(exports =>
-        exports.collect { case e @ (_, VarModel(_, StreamType(_), _)) =>
-          e
-        }
+    getOutsideStreamNames.flatMap { outsideDeclaredStreams =>
+      // Function's internal variables will not be available outside, hence the scope
+      Exports[S].scope(
+        for {
+          // Process renamings, prepare environment
+          tr <- prelude[S](fn, call)
+          (tree, results) = tr
+
+          // Register captured values as available exports
+          _ <- Exports[S].resolved(fn.capturedValues)
+          _ <- Mangler[S].forbid(fn.capturedValues.keySet)
+
+          // Now, substitute the arrows that were received as function arguments
+          // Use the new op tree (args are replaced with values, names are unique & safe)
+          callableFuncBodyNoTopology <- TagInliner.handleTree(tree, fn.funcName)
+          callableFuncBody =
+            fn.capturedTopology
+              .fold[OpModel](SeqModel)(ApplyTopologyModel.apply)
+              .wrap(callableFuncBodyNoTopology)
+
+          opsAndRets <- pushStreamResults(outsideDeclaredStreams, call.exportTo, results, callableFuncBody)
+          (ops, rets) = opsAndRets
+        } yield SeqModel.wrap(ops.reverse: _*) -> rets.reverse
       )
-      .flatMap { outsideDeclaredStreams =>
-        // Function's internal variables will not be available outside, hence the scope
-        Exports[S].scope(
-          for {
-            // Process renamings, prepare environment
-            tr <- prelude[S](fn, call)
-            (tree, result) = tr
-
-            // Register captured values as available exports
-            _ <- Exports[S].resolved(fn.capturedValues)
-            _ <- Mangler[S].forbid(fn.capturedValues.keySet)
-
-            // Now, substitute the arrows that were received as function arguments
-            // Use the new op tree (args are replaced with values, names are unique & safe)
-            callableFuncBodyNoTopology <- TagInliner.handleTree(tree, fn.funcName)
-            callableFuncBody =
-              fn.capturedTopology
-                .fold[OpModel](SeqModel)(ApplyTopologyModel.apply)
-                .wrap(callableFuncBodyNoTopology)
-
-            // Fix return values with exports collected in the body
-            resolvedResult <- RawValueInliner.valueListToModel(result)
-            // Fix the return values
-            (ops, rets) = (call.exportTo zip resolvedResult)
-              .map[(List[OpModel.Tree], ValueModel)] {
-                case (
-                      CallModel.Export(n, StreamType(_)),
-                      (res @ VarModel(_, StreamType(_), _), resDesugar)
-                    ) if !outsideDeclaredStreams.contains(n) =>
-                  resDesugar.toList -> res
-                case (CallModel.Export(exp, st @ StreamType(_)), (res, resDesugar)) =>
-                  // pass nested function results to a stream
-                  (resDesugar.toList :+ PushToStreamModel(
-                    res,
-                    CallModel.Export(exp, st)
-                  ).leaf) -> VarModel(
-                    exp,
-                    st,
-                    Chain.empty
-                  )
-                case (_, (res, resDesugar)) =>
-                  resDesugar.toList -> res
-              }
-              .foldLeft[(List[OpModel.Tree], List[ValueModel])](
-                (callableFuncBody :: Nil, Nil)
-              ) { case ((ops, rets), (fo, r)) =>
-                (fo ::: ops, r :: rets)
-              }
-          } yield SeqModel.wrap(ops.reverse: _*) -> rets.reverse
-        )
-      }
+    }
 
   /**
    * Prepare the state context for this function call
@@ -107,30 +123,35 @@ object ArrowInliner extends Logging {
       // Collect all arguments: what names are used inside the function, what values are received
       argsFull <- State.pure(ArgsCall(fn.arrowType.domain, call.args))
 
+      streamArgs = argsFull.streamArgs
+
       // DataType arguments
-      argsToDataRaw = argsFull.dataArgs
+      argsToDataRaw = argsFull.nonStreamDataArgs ++ streamArgs
 
       // Arrow arguments: expected type is Arrow, given by-name
       argsToArrowsRaw <- Arrows[S].argsArrows(argsFull)
 
       // collect arguments with stream type
       // to exclude it from resolving and rename it with a higher-level stream that passed by argument
-      // TODO: what if we have streams in property???
-      streamToRename = argsFull.streamArgs.view.mapValues(_.name).toMap
+      streamToRename = streamArgs.view.mapValues(_.name).toMap
 
       // Find all duplicates in arguments
       // we should not rename arguments that will be renamed by 'streamToRename'
-      argsShouldRename <- Mangler[S].findNewNames(
-        argsToDataRaw.keySet ++ argsToArrowsRaw.keySet -- streamToRename.keySet
+      argsToDataShouldRename <- Mangler[S].findNewNames(
+        argsToDataRaw.keySet
+      )
+      argsToArrowsShouldRename <- Mangler[S].findNewNames(
+        argsToArrowsRaw.keySet
       )
 
       // Do not rename arguments if they just match external names
       argsToData = argsToDataRaw.map { case (k, v) =>
-        argsShouldRename.getOrElse(k, k) -> v
+        argsToDataShouldRename.getOrElse(k, k) -> v
       }
+
       _ <- Exports[S].resolved(argsToData)
 
-      argsToArrows = argsToArrowsRaw.map { case (k, v) => argsShouldRename.getOrElse(k, k) -> v }
+      argsToArrows = argsToArrowsRaw.map { case (k, v) => argsToArrowsShouldRename.getOrElse(k, k) -> v }
 
       returnedArrows = fn.ret.collect { case VarRaw(name, ArrowType(_, _)) =>
         name
@@ -144,6 +165,8 @@ object ArrowInliner extends Logging {
       // Going to resolve arrows: collect them all. Names should never collide: it's semantically checked
       _ <- Arrows[S].purge
       _ <- Arrows[S].resolved(renamedCapturedArrows ++ argsToArrows)
+
+      argsShouldRename = argsToDataShouldRename ++ argsToArrowsShouldRename -- streamToRename.keySet
 
       // Rename all renamed arguments in the body
       treeRenamed =
