67e262069c6dbf55f8854df86b106d4f75651cc4
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-06-29 13:51:20 +0300

    add test and fix inliner

--- model/inline/src/main/scala/aqua/model/inline/ArrowInliner.scala
+++ model/inline/src/main/scala/aqua/model/inline/ArrowInliner.scala
@@ -45,7 +45,7 @@ object ArrowInliner extends Logging {
       resolvedResult <- RawValueInliner.valueListToModel(results)
     } yield {
       // Fix the return values
-      (exportTo zip resolvedResult).map {
+      val (ops, rets) = (exportTo zip resolvedResult).map {
         case (
               CallModel.Export(n, StreamType(_)),
               (res @ VarModel(_, StreamType(_), _), resDesugar)
@@ -63,11 +63,9 @@ object ArrowInliner extends Logging {
           )
         case (_, (res, resDesugar)) =>
           resDesugar.toList -> res
-      }.foldLeft[(List[OpModel.Tree], List[ValueModel])](
-        (body :: Nil, Nil)
-      ) { case ((ops, rets), (fo, r)) =>
-        (fo ::: ops, r :: rets)
-      }
+      }.unzip.leftMap(_.flatten)
+
+      (ops :+ body, rets)
     }
 
   // Apply a callable function, get its fully resolved body & optional value, if any
