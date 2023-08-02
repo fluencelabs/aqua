06e604428f3c1dec22483bd5296f2f463e769926
InversionSpaces <InversionSpaces@vivaldi.net>
2023-08-02 08:37:44 +0000

    Remove comment

--- model/inline/src/main/scala/aqua/model/inline/raw/ApplyBinaryOpRawInliner.scala
+++ model/inline/src/main/scala/aqua/model/inline/raw/ApplyBinaryOpRawInliner.scala
@@ -101,7 +101,6 @@ object ApplyBinaryOpRawInliner extends RawInliner[ApplyBinaryOpRaw] {
     val predo = (resName: String) =>
       SeqModel.wrap(
         linline.predo ++ rinline.predo :+ XorModel.wrap(
-          // TODO: Canonicalize values if they are streams
           MatchMismatchModel(lmodel, rmodel, shouldMatch).wrap(
             FlattenModel(
               LiteralModel.bool(true),
