ef4b0143ac7cd4e1a5997d6a0f1f690ab806a315
Dima <dmitry.shakhtarin@fluence.ai>
2023-08-09 15:21:18 +0200

    fix(compiler): Runtime error on compilation exported functions with top types [fixes LNG-218] (#822)

--- backend/ts/src/main/scala/aqua/backend/ts/TypeScriptCommon.scala
+++ backend/ts/src/main/scala/aqua/backend/ts/TypeScriptCommon.scala
@@ -37,13 +37,26 @@ object TypeScriptCommon {
       s"{ ${st.fields.map(typeToTs).toNel.map(kv => kv._1 + ": " + kv._2 + ";").toList.mkString(" ")} }"
     case st: AbilityType =>
       s"{ ${st.fields.map(typeToTs).toNel.map(kv => kv._1 + ": " + kv._2 + ";").toList.mkString(" ")} }"
-    case st: ScalarType if ScalarType.number(st) => "number"
-    case ScalarType.bool => "boolean"
-    case ScalarType.string => "string"
-    case lt: LiteralType if lt.oneOf.exists(ScalarType.number) => "number"
-    case lt: LiteralType if lt.oneOf(ScalarType.bool) => "boolean"
-    case lt: LiteralType if lt.oneOf(ScalarType.string) => "string"
+    case st: ScalarType => st match {
+      case st: ScalarType if ScalarType.number(st) => "number"
+      case ScalarType.bool => "boolean"
+      case ScalarType.string => "string"
+      // unreachable
+      case _ => "any"
+    }
+    case lt: LiteralType => lt match {
+      case lt: LiteralType if lt.oneOf.exists(ScalarType.number) => "number"
+      case lt: LiteralType if lt.oneOf(ScalarType.bool) => "boolean"
+      case lt: LiteralType if lt.oneOf(ScalarType.string) => "string"
+      // unreachable
+      case _ => "any"
+    }
     case at: ArrowType => fnDef(at)
+    case TopType => "any"
+    case BottomType => "nothing"
+
+    // impossible. Made to avoid compilation warning
+    case t: CanonStreamType => "any"
   }
 
   // TODO: handle cases if there is already peer_ or config_ variable defined
