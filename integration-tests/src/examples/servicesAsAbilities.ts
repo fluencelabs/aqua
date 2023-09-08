8b1f094a56c8deec85c336aad599e9240e61ab96
InversionSpaces <InversionSpaces@vivaldi.net>
2023-09-08 13:43:46 +0000

    Refactor test

--- integration-tests/src/examples/servicesAsAbilities.ts
+++ integration-tests/src/examples/servicesAsAbilities.ts
@@ -1,17 +1,23 @@
 import { test, registerTestService } from '../compiled/examples/servicesAsAbilities.js';
 
-const serviceIds = ['default-id', 'resolved-id-1', 'resolved-id-2', 'iter-id-1', 'iter-id-2'];
+const serviceIds = {
+    default: 'default-id',
+    resolved1: 'resolved-id-1',
+    resolved2: 'resolved-id-2',
+    iter1: 'iter-id-1',
+    iter2: 'iter-id-2',
+};
 
 const serviceIdsSequence = [
-    serviceIds[0], // dafault
-    serviceIds[0], // default
-    serviceIds[1], // resolved 1
-    serviceIds[1], // resolved 1
-    serviceIds[0], // default
-    serviceIds[3], // iter 1
-    serviceIds[4], // iter 2
-    serviceIds[1], // resolved 1
-    serviceIds[2], // resolved 2
+    serviceIds.default,
+    serviceIds.default,
+    serviceIds.resolved1,
+    serviceIds.resolved1,
+    serviceIds.default,
+    serviceIds.iter1,
+    serviceIds.iter2,
+    serviceIds.resolved1,
+    serviceIds.resolved2,
 ];
 
 const msgs = ['call', 'capture call', 'accept closure call', 'accept ability call'];
@@ -19,7 +25,7 @@ const msgs = ['call', 'capture call', 'accept closure call', 'accept ability cal
 export const expectedServiceResults = serviceIdsSequence.flatMap((id) => msgs.map((msg) => `${id}: ${msg}`));
 
 export async function servicesAsAbilitiesCall() {
-    serviceIds.forEach((id) =>
+    Object.entries(serviceIds).forEach(([_key, id], _idx) =>
         registerTestService(id, {
             concatId: (s: string) => {
                 return `${id}: ${s}`;
