0bd9791dc258639fc4a4d07ad86a37e3efa8e80d
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-09-13 11:57:26 +0200

    add code to run aqua

--- api/api/.jvm/src/main/scala/aqua/api/Test.scala
+++ api/api/.jvm/src/main/scala/aqua/api/Test.scala
@@ -1,18 +1,39 @@
 package aqua.api
-import cats.effect.{IO, IOApp}
-import aqua.backend.js.JavaScriptBackend
+
+import aqua.api.TargetType.TypeScriptType
 import aqua.backend.ts.TypeScriptBackend
-import aqua.api.TargetType.JavaScriptType
+import aqua.compiler.AquaCompiled
+import aqua.files.FileModuleId
+import cats.data.Chain
+import cats.data.Validated.{Invalid, Valid}
+import cats.effect.{IO, IOApp}
+import fs2.io.file.{Files, Path}
+import fs2.{Stream, text}
 
 object Test extends IOApp.Simple {
+
   override def run: IO[Unit] = {
-    val input =
-      """func getNumber(number: u32) -> u32:
-        |    <- number
-        |""".stripMargin
-    APICompilation.compileString(input, Nil, AquaAPIConfig(targetType = JavaScriptType), JavaScriptBackend()).map {
-      res =>
-        println(res)
-    }
+    APICompilation
+      .compilePath(
+        "./aqua-src/antithesis.aqua",
+        "./aqua" :: Nil,
+        AquaAPIConfig(targetType = TypeScriptType),
+        TypeScriptBackend(false, "IFluenceClient$$")
+      )
+      .flatMap {
+        case Valid(res) =>
+          val content = res.get(0).get.compiled.head.content
+          val targetPath =  Path("./target/antithesis.ts")
+
+          Stream.emit(content)
+            .through(text.utf8.encode)
+            .through(Files[IO].writeAll(targetPath))
+            .attempt
+            .compile
+            .last.map(_ => println(s"File: ${targetPath.absolute.normalize}"))
+        case Invalid(e) =>
+          IO.delay(println(e))
+
+      }
   }
 }
