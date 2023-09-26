eaaa956a8d711de6c06c1f896adcf9788acd5783
InversionSpaces <InversionSpaces@vivaldi.net>
2023-09-26 09:47:18 +0000

    Fix integration tests

--- integration-tests/src/__test__/examples.spec.ts
+++ integration-tests/src/__test__/examples.spec.ts
@@ -746,7 +746,7 @@ describe("Testing examples", () => {
   });
 
   it("errorClear.aqua", async () => {
-    let errorClearResult = await errorClearCall(relayPeerId2);
+    let errorClearResult = await errorClearCall(peer2);
     expect(errorClearResult).toEqual(0);
   });
 
--- integration-tests/src/examples/errorClear.ts
+++ integration-tests/src/examples/errorClear.ts
@@ -1,15 +1,16 @@
+import { IFluenceClient } from "@fluencelabs/js-client";
 import {
   errorClearTest,
   registerFailService,
 } from "../compiled/examples/errorClear.js";
 
-export async function errorClearCall(node: string) {
-  registerFailService({
+export async function errorClearCall(peer: IFluenceClient) {
+  registerFailService(peer, {
     call: (s: string) => {
       if (s === "fail") return Promise.reject(s);
       else return Promise.resolve(s);
     },
   });
 
-  return await errorClearTest(node);
+  return await errorClearTest(peer.getPeerId(), peer.getRelayPeerId());
 }
