cee444862a7cf2bea3f84e921b03328fd83fa108
InversionSpaces <InversionSpaces@vivaldi.net>
2023-11-17 11:43:13 +0100

    feat(compiler): Generate empty calls to `responseHandlerSrv` [LNG-286] (#979)
    * Add noEmptyResponse
    
    * Fix tests

--- model/transform/src/main/scala/aqua/model/transform/TransformConfig.scala
+++ model/transform/src/main/scala/aqua/model/transform/TransformConfig.scala
@@ -1,19 +1,33 @@
 package aqua.model.transform
 
 import aqua.model.{AquaContext, LiteralModel, ValueModel, VarModel}
-import aqua.raw.{ConstantRaw, RawContext, RawPart}
 import aqua.raw.value.{LiteralRaw, ValueRaw, VarRaw}
+import aqua.raw.{ConstantRaw, RawContext, RawPart}
 import aqua.types.ScalarType
+
 import cats.data.Chain
 import cats.kernel.Monoid
 
-// TODO docs
+/**
+ * Configuration for function pre transformer
+ *
+ * @param getDataService - name of the service that provides arguments
+ * @param callbackService - name of the service that provides callbacks
+ * @param errorHandlingService - name of the service that handles errors
+ * @param errorFuncName - name of the function that handles errors (in errorHandlingService)
+ * @param respFuncName - name of the function that handles responses (in getDataService)
+ * @param noEmptyResponse - if true, do not generate response call if there is no return values
+ * @param relayVarName - name of the relay variable
+ * @param tracing - tracing configuration
+ * @param constants - list of constants
+ */
 case class TransformConfig(
   getDataService: String = "getDataSrv",
   callbackService: String = "callbackSrv",
   errorHandlingService: String = "errorHandlingSrv",
   errorFuncName: String = "error",
   respFuncName: String = "response",
+  noEmptyResponse: Boolean = false,
   relayVarName: Option[String] = Some("-relay-"),
   tracing: Option[TransformConfig.TracingConfig] = None,
   constants: List[ConstantRaw] = Nil
