4a523ae3fb591d27e4d918b3b62b04815bf59fe6
InversionSpaces <inversionspaces@vivaldi.net>
2023-12-18 16:44:56 +0000

    Fix import

--- integration-tests/src/examples/recursiveStreams/range.ts
+++ integration-tests/src/examples/recursiveStreams/range.ts
@@ -1,4 +1,4 @@
-import { range } from "../../compiled/examples/fork/range.js";
+import { range } from "../../compiled/examples/recursiveStreams/range.js";
 
 export async function rangeCall(a: number, b: number): Promise<number[]> {
   return await range(a, b);
