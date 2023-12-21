a37895e15dc876f18b7b085d36d236be47d64395
InversionSpaces <inversionspaces@vivaldi.net>
2023-12-21 09:29:33 +0000

    Refactor

--- io/src/main/scala/aqua/SpanParser.scala
+++ io/src/main/scala/aqua/SpanParser.scala
@@ -3,29 +3,27 @@ package aqua
 import aqua.files.FileModuleId
 import aqua.parser.lift.{FileSpan, Span}
 import aqua.parser.{Ast, Parser, ParserError}
+
 import cats.data.*
 import cats.parse.LocationMap
-import cats.{~>, Comonad, Eval, Monad, Monoid, Order}
+import cats.{Comonad, Eval, Monad, Monoid, Order, ~>}
 
 object SpanParser extends scribe.Logging {
 
-  def parser: FileModuleId => String => ValidatedNec[ParserError[FileSpan.F], Ast[FileSpan.F]] = {
+  def parser: FileModuleId => String => ValidatedNec[ParserError[FileSpan.F], Ast[FileSpan.F]] =
     id =>
-      { source =>
-        {
-          logger.trace(s"creating parser for $id...")
-          val nat = new (Span.S ~> FileSpan.F) {
-            override def apply[A](span: Span.S[A]): FileSpan.F[A] = {
-              (
-                FileSpan(id.file.absolute.toString, Eval.later(LocationMap(source)), span._1),
-                span._2
-              )
-            }
+      source => {
+        logger.trace(s"creating parser for $id...")
+        val nat = new (Span.S ~> FileSpan.F) {
+          override def apply[A](span: Span.S[A]): FileSpan.F[A] = {
+            (
+              FileSpan(id.file.absolute.toString, Eval.later(LocationMap(source)), span._1),
+              span._2
+            )
           }
-          val parser = Parser.natParser(Parser.spanParser, nat)(source)
-          logger.trace("parser created")
-          parser
         }
+        val parser = Parser.natParser(Parser.spanParser, nat)(source)
+        logger.trace("parser created")
+        parser
       }
-  }
 }
