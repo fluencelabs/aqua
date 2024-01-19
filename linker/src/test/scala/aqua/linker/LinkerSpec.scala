85a9438e70d37e695993f5579f8e2f206e005062
InversionSpaces <inversionspaces@vivaldi.net>
2024-01-19 17:42:22 +0000

    Fix unit test

--- linker/src/test/scala/aqua/linker/LinkerSpec.scala
+++ linker/src/test/scala/aqua/linker/LinkerSpec.scala
@@ -1,44 +1,62 @@
 package aqua.linker
 
-import cats.data.Validated
+import cats.Id
+import cats.data.{EitherNec, NonEmptyChain}
+import cats.syntax.either.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class LinkerSpec extends AnyFlatSpec with Matchers {
 
+  type TP = Map[String, String] => EitherNec[String, String]
+
+  val cycle: NonEmptyChain[String] => String =
+    _.toChain.toList.mkString(" -> ")
+
   "linker" should "resolve dependencies" in {
 
-    val empty = Modules[String, String, String => String]()
+    val empty = Modules[String, String, TP]()
 
-    val withMod1 =
-      empty
-        .add(
-          AquaModule[String, String, String => String](
-            id = "mod1",
-            imports = Map.empty,
-            dependsOn = Map("mod2" -> "unresolved mod2 in mod1"),
-            body = _ ++ " | mod1"
-          ),
-          toExport = true
-        )
+    val withMod1 = empty.add(
+      AquaModule(
+        id = "mod1",
+        imports = Map("mod2" -> "mod2"),
+        dependsOn = Map("mod2" -> "unresolved mod2 in mod1"),
+        body = imports => {
+          println(s"mod1: $imports")
+
+          imports
+            .get("mod2")
+            .toRight("mod2 not found in mod1")
+            .toEitherNec
+            .map(_ ++ " | mod1")
+        }
+      ),
+      toExport = true
+    )
     withMod1.isResolved should be(false)
 
-    Linker.link[String, String, String](
-      withMod1,
-      cycle => cycle.map(_.id).toChain.toList.mkString(" -> "),
-      _ => ""
-    ) should be(Validated.invalidNec("unresolved mod2 in mod1"))
+    Linker.link(withMod1, cycle) should be(
+      Left("unresolved mod2 in mod1").toEitherNec
+    )
 
-    val withMod2 =
-      withMod1.add(AquaModule("mod2", Map.empty, Map.empty, _ ++ " | mod2"))
+    val withMod2 = withMod1.add(
+      AquaModule(
+        id = "mod2",
+        imports = Map.empty,
+        dependsOn = Map.empty,
+        body = _ => "mod2".asRight.toEitherNec
+      )
+    )
 
     withMod2.isResolved should be(true)
 
-    Linker.link[String, String, String](
-      withMod2,
-      cycle => cycle.map(_.id + "?").toChain.toList.mkString(" -> "),
-      _ => ""
-    ) should be(Validated.validNec(Map("mod1" -> " | mod2 | mod1")))
+    Linker.link(withMod2, cycle) should be(
+      Map(
+        "mod1" -> "mod2 | mod1",
+        "mod2" -> "mod2"
+      ).asRight.toEitherNec
+    )
   }
 
 }
