184ee2a594a0ab94809c7310a7bd18a2a21e286c
InversionSpaces <InversionSpaces@vivaldi.net>
2024-03-20 11:03:19 +0000

    Delete streamsDefinedWithinScope

--- semantics/src/main/scala/aqua/semantics/rules/names/NamesAlgebra.scala
+++ semantics/src/main/scala/aqua/semantics/rules/names/NamesAlgebra.scala
@@ -2,12 +2,13 @@ package aqua.semantics.rules.names
 
 import aqua.parser.lexer.{LiteralToken, Name, Token, ValueToken}
 import aqua.types.{ArrowType, StreamType, Type}
+
 import cats.InjectK
 
 trait NamesAlgebra[S[_], Alg[_]] {
 
   def read(name: Name[S], mustBeDefined: Boolean = true): Alg[Option[Type]]
-  
+
   // TODO can be implemented via read?
   def constantDefined(name: Name[S]): Alg[Option[Type]]
 
@@ -24,8 +25,6 @@ trait NamesAlgebra[S[_], Alg[_]] {
   def defineConstant(name: Name[S], `type`: Type): Alg[Boolean]
 
   def defineArrow(name: Name[S], gen: ArrowType, isRoot: Boolean): Alg[Boolean]
-
-  def streamsDefinedWithinScope(): Alg[Map[String, StreamType]]
 
   def beginScope(token: Token[S]): Alg[Unit]
 
