6cc068ac36a6af2a96a793278c8f721c8165b67d
Dima <dmitry.shakhtarin@fluence.ai>
2024-05-07 12:58:37 +0300

    fix(lsp): Plugin throws OOM on big projects (#1134)

--- semantics/src/main/scala/aqua/semantics/rules/locations/LocationsState.scala
+++ semantics/src/main/scala/aqua/semantics/rules/locations/LocationsState.scala
@@ -4,37 +4,27 @@ import aqua.helpers.syntax.list.*
 import aqua.parser.lexer.Token
 
 import cats.kernel.Monoid
+import cats.syntax.semigroup.*
 import scribe.Logging
 
 case class LocationsState[S[_]](
-  variables: List[VariableInfo[S]] = Nil
+  variables: Variables[S] = Variables[S]()
 ) extends Logging {
 
-  lazy val allLocations: List[TokenLocation[S]] = variables.flatMap(_.allLocations)
+  lazy val allLocations: List[TokenLocation[S]] = variables.allLocations
 
-  def addDefinitions(newDefinitions: List[DefinitionInfo[S]]): LocationsState[S] =
-    copy(variables = newDefinitions.map(d => VariableInfo(d)) ++ variables)
+  def addDefinitions(newDefinitions: List[DefinitionInfo[S]]): LocationsState[S] = {
+    copy(variables = variables.addDefinitions(newDefinitions))
+  }
 
   def addDefinition(newDef: DefinitionInfo[S]): LocationsState[S] =
-    copy(variables = VariableInfo(newDef) +: variables)
-
-  private def addOccurrenceToFirst(
-    vars: List[VariableInfo[S]],
-    name: String,
-    token: Token[S]
-  ): List[VariableInfo[S]] = {
-    // TODO: this code lasts too long, but we can find errors in it.
-    // if (!vars.exists(_.definition.name == name))
-    //   logger.error(s"Unexpected. Cannot add occurrence for $name")
-
-    vars.updateFirst(_.definition.name == name, v => v.copy(occurrences = token +: v.occurrences))
-  }
+    copy(variables = variables.addDefinitions(newDef :: Nil))
 
   def addLocation(
     name: String,
     token: Token[S]
   ): LocationsState[S] =
-    copy(variables = addOccurrenceToFirst(variables, name, token))
+    copy(variables = variables.addOccurence(name, token))
 
   def addLocations(
     locations: List[(String, Token[S])]
@@ -47,11 +37,11 @@ case class LocationsState[S[_]](
 object LocationsState {
 
   given [S[_]]: Monoid[LocationsState[S]] with {
-    override def empty: LocationsState[S] = LocationsState()
+    override def empty: LocationsState[S] = LocationsState[S]()
 
     override def combine(x: LocationsState[S], y: LocationsState[S]): LocationsState[S] =
       LocationsState(
-        variables = x.variables ++ y.variables
+        variables = x.variables.combine(y.variables)
       )
   }
 }
--- utils/helpers/src/main/scala/aqua/helpers/syntax/list.scala
+++ utils/helpers/src/main/scala/aqua/helpers/syntax/list.scala
@@ -3,7 +3,9 @@ package aqua.helpers.syntax
 import scala.annotation.tailrec
 
 object list {
-  extension[A] (l: List[A]) {
+
+  extension [A](l: List[A]) {
+
     def updateFirst[B >: A](p: A => Boolean, f: A => B): List[B] = {
       @tailrec
       def update(left: List[B], right: List[A]): List[B] =
