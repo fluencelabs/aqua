77f63c74e5c5c6a7c6d7ff35ab5f6957ced7ec80
DieMyst <dmitry.shakhtarin@fluence.ai>
2024-05-30 14:47:09 +0700

    fix compilation

--- integration-tests/src/examples/parSeqCall.ts
+++ integration-tests/src/examples/parSeqCall.ts
@@ -3,6 +3,8 @@ import {
 } from "../compiled/examples/parseq.js";
 import { config } from "../config.js";
 
+const relays = config.relays;
+
 export async function testParSeqCall() {
     return testParSeq(relays[3].peerId, relays[4].peerId, relays[5].peerId)
 }
