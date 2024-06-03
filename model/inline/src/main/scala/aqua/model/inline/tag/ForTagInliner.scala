e314f066fb6b863c6b5123fc3fbdc5f612300a9b
Dima <dmitry.shakhtarin@fluence.ai>
2024-06-03 19:55:35 +0300

    Update model/inline/src/main/scala/aqua/model/inline/tag/ForTagInliner.scala
    Co-authored-by: InversionSpaces <InversionSpaces@vivaldi.net>

--- model/inline/src/main/scala/aqua/model/inline/tag/ForTagInliner.scala
+++ model/inline/src/main/scala/aqua/model/inline/tag/ForTagInliner.scala
@@ -44,21 +44,23 @@ final case class ForTagInliner(
     }
     itemVar = VarModel(n, elementType)
     _ <- Exports[S].resolved(item, itemVar)
-    pref <- keyValue match {
-      case None => State.pure(None)
-      case Some(kv) =>
-        for {
-          keyName <- Mangler[S].findAndForbidName(kv.key)
-          _ <- Exports[S].resolved(kv.key, VarModel(keyName, ScalarType.string))
-          valueName <- Mangler[S].findAndForbidName(kv.value)
-          _ <- Exports[S].resolved(kv.value, VarModel(valueName, elementType))
-        } yield {
-          Some(SeqModel.wrap(
-            FlattenModel(itemVar.withProperty(IntoFieldModel("key", ScalarType.string)), keyName).leaf,
-            FlattenModel(itemVar.withProperty(IntoFieldModel("value", elementType)), valueName).leaf
-          ))
-        }
-    }
+    pref <- keyValue.traverse(kv =>
+      for {
+        keyName <- Mangler[S].findAndForbidName(kv.key)
+        _ <- Exports[S].resolved(kv.key, VarModel(keyName, ScalarType.string))
+        valueName <- Mangler[S].findAndForbidName(kv.value)
+        _ <- Exports[S].resolved(kv.value, VarModel(valueName, elementType))
+      } yield SeqModel.wrap(
+        FlattenModel(
+          itemVar.withProperty(IntoFieldModel("key", ScalarType.string)),
+          keyName
+        ).leaf,
+        FlattenModel(
+          itemVar.withProperty(IntoFieldModel("value", elementType)),
+          valueName
+        ).leaf
+      )
+    )
     modeModel = mode match {
       case ForTag.Mode.SeqMode | ForTag.Mode.TryMode => ForModel.Mode.Null
       case ForTag.Mode.ParMode | ForTag.Mode.RecMode => ForModel.Mode.Never
