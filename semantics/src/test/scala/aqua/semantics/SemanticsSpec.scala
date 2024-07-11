7a618cbac868017abaa36925301ca74304ce422c
InversionSpaces <inversionspaces@vivaldi.net>
2024-07-11 14:50:16 +0000

    Add test

--- semantics/src/test/scala/aqua/semantics/SemanticsSpec.scala
+++ semantics/src/test/scala/aqua/semantics/SemanticsSpec.scala
@@ -1012,4 +1012,24 @@ class SemanticsSpec extends AnyFlatSpec with Matchers with Inside {
       atLeast(1, errors.toChain.toList) shouldBe a[RulesViolated[Span.S]]
     }
   }
+
+  it should "prohibit putting stream maps into collection" in {
+    def test(prefix: String = "") = {
+      val script = s"""|func test(arr: []⊤) -> string:
+                       |  <- "test"
+                       |
+                       |func main() -> string:
+                       |  map: %string
+                       |  <- test(${prefix}[map])""".stripMargin
+
+      insideSemErrors(script) { errors =>
+        errors.collect { case RulesViolated(_, messages) => messages }.toList.flatten
+          .contains("Value of type '%string' could not be put into a collection") shouldBe (true)
+      }
+    }
+
+    test()
+    test("?")
+    test("*")
+  }
 }
